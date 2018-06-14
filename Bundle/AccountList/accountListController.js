4d40cff5790021f802b279ef4f0830ac5ac100aa
ssing902 <sukhpartap55@yahoo.com>
2018-06-14 16:04:04 -0400

    update accountList component

--- Bundle/AccountList/accountListController.js
+++ Bundle/AccountList/accountListController.js
@@ -1,7 +1,7 @@
 ({
 	doInit : function(component, event, helper) {
         // Retreive all accounts during component intializtion
-        helper.loadAccount(component);
+        helper.loadAccounts(component);
         
         var accounts = component.get("v.accounts");
         var accountList = component.get("v.accountList");
