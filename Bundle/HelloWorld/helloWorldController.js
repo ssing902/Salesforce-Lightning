2eb7c88259aa94c85873859651c52d5f6334ea02
ssing902 <sukhpartap55@yahoo.com>
2018-07-06 12:05:50 -0400

    navigateToSObject

--- Bundle/HelloWorld/helloWorldController.js
+++ Bundle/HelloWorld/helloWorldController.js
@@ -2,6 +2,14 @@
 	doInit : function(component, event, helper) {
 		//var globalId = cmp.getGlobalId();
         console.log(/*globalId + */" Hello World");
+    },
+    handleClick:function(component,event,helper)
+    {
+        var navEvt = $A.get("e.force:navigateToSObject");
+        navEvt.setParams({
+            "recordId":component.get("v.recordId")
+        });
+        navEvt.fire();
     }
     
 })
